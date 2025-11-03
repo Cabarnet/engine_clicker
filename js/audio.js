f2ec60851d48f68a583ddcc8e0642f817b63d1e0
Ахметов Руслан <cabarnet713@gmail.com>
2025-11-03 23:31:55 +0300

    Fix

--- js/audio.js
+++ js/audio.js
@@ -1,5 +1,5 @@
 let firstTouch = true;
-const backgroundAudio = new Audio('../audio/Bicycle Ride - Jeremy Korpas.mp3');
+const backgroundAudio = new Audio('audio/Bicycle Ride - Jeremy Korpas.mp3');
 
 window.addEventListener('click', function() {
     if (firstTouch) {
@@ -15,13 +15,13 @@ document.querySelector('#workers-view-btn').addEventListener('click', function()
 });
 
 function clickSound() {
-    const audio = new Audio('../audio/click.mp3');
+    const audio = new Audio('audio/click.mp3');
     audio.volume = 0.2;
     audio.play();
 }
 
 function buySound() {
-    const audio = new Audio('../audio/Selled.mp3');
+    const audio = new Audio('audio/Selled.mp3');
     audio.volume = 0.2;
     audio.play();
 }
